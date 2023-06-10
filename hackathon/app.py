68df707775de38182ada3fb85e8e3c507d4935ac
Amartya Jha <amartyaj@zeta.tech>
2023-06-11 01:45:37 +0530

    adding-unit-test-code

--- hackathon/app.py
+++ hackathon/app.py
@@ -5,7 +5,7 @@ def get_product_list():
     response = requests.get(url)
     if response.status_code == 200:
         product_list = response.json()
-        return product_list.get("products", [])
+        return product_list.get("product", [])
     else:
         print("Error: Failed to retrieve product list")
         return []
