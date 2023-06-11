490ff42fc784b30e3b0b9e235c37c4b203e7dcbb
Amartya Jha <amartyaj@zeta.tech>
2023-06-11 12:44:52 +0530

    Adding doc

--- hackathon/app.py
+++ hackathon/app.py
@@ -2,7 +2,7 @@ import requests
 
 def get_product_list():
     """This function retrieves a list of products from a dummy API and returns it, or an empty list if the API call fails."""
-    openAI_key="7yY9eBigr44EAA1RZ9JaT3BlbkFJzxUsDCR4ELWyoBSe7tfb"
+    
     url = "https://dummyjson.com/products"
     response = requests.get(url)
     if response.status_code == 200:
